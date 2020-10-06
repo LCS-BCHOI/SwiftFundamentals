c635425cce5de7a53ef98052b43981bfc16f1afe
Bosco Choi <bchoi@lcs.on.ca>
2020-10-06 14:58:33 -0400

    Learning all about struct and class and functions

--- Day8.playground/Contents.swift
+++ Day8.playground/Contents.swift
@@ -10,3 +10,48 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+
+// Making a structs
+struct Student{
+    var name:String
+    var id: Int{ // This is to be set to seom value before swift can detech soemthing changed
+        didSet{
+            print("Student id has changed")
+        }
+    }
+    
+    mutating func change_name(name: String) {// Define a method
+        self.name = names // accessing to a local variable
+    }
+    
+    // Changing a variables that is in struc
+    mutating func change(){
+        name = "The name has changed"
+    }
+}
+
+var a = TheBigMath( name: "Bill Ting", id: 311)
+a.value = 23
+a.value = 13
+
+print(a.change_name("New Name"))
+print(a.name)
+
+let string = "This is a test string so that I could count how many string character there are"
+
+print(string.count)
+
+print(string.lowercased())
+
+print(string.uppercased())
+print(string.sorted())
+
+var test = string.sorted()
+
+test.append(contentsOf: "Hello I am Bosco")
+
+test.firstIndex(of: "I")
+
+print(test)
+
+test.remove(at: 0)
