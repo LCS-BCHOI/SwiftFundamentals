a12a18fe86bd7ddb2ba67f73e53fd722782d1e07
Bosco Choi <bchoi@lcs.on.ca>
2020-10-06 14:49:36 -0400

    Learned everything about a function. I think it is really interesting, I've never leaned something that interesting.

--- Day5.playground/Contents.swift
+++ Day5.playground/Contents.swift
@@ -10,3 +10,27 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+
+func helloworld() { // defined a function
+    print("Hello World")
+}
+
+helloworld()
+
+func one() -> Int{ // A function that returns an int
+    return 1
+}
+
+print(one())
+
+func twovaluefunction() -> (firstname :String, lastname :String) { // A function that retuns two value
+    return (firstname: "Vincent", lastname: "He")
+}
+
+print(twovaluefunction())
+
+// A function that takes in a value
+func returnint(n1: Int) -> Int {
+    return n1
+}
+print(returnint(n1: 30))
