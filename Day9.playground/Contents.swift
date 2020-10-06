bfc09590821b0ee4c3be6670363c8c9d8b7ab554
Bosco Choi <bchoi@lcs.on.ca>
2020-10-06 15:03:09 -0400

    Structure and stuff that I've learned

--- Day9.playground/Contents.swift
+++ Day9.playground/Contents.swift
@@ -10,3 +10,45 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+
+// Swift another cool things
+struct FamilyTree{
+    init(){
+        print("Creating family tree")
+    }
+    
+}
+
+struct Student{
+
+    private var id:Double // A private variables in swift
+    lazy var familytree = FamilyTree() // Only created when family tree is accsessed
+    
+    init(student_name:String){
+        self.name = student_name
+    }
+    
+}
+    
+
+// Another Example
+struct User {
+    var username: String
+
+    init() {
+        username = "Anonymous"
+        print("Creating a new user!")
+    }
+}
+var user = User()
+user.username = "twostraws"
+
+//refering to the current instance
+struct Person {
+    var name: String
+
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+    }
+}
